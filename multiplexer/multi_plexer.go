99f10d0893eaa79f148c5f501829f354503cab5b
wada <wada0421514@gmail.com>
2024-06-26 00:54:56 +0900

    16to1 8bit multi plexerの追加

--- multiplexer/multi_plexer.go
+++ multiplexer/multi_plexer.go
@@ -45,6 +45,17 @@ type bool4bit struct {
 	b0 bool
 }
 
+type bool8bit struct {
+	b7 bool
+	b6 bool
+	b5 bool
+	b4 bool
+	b3 bool
+	b2 bool
+	b1 bool
+	b0 bool
+}
+
 // 16to1マルチプレクサ(1bit)
 func Multiplexer16to1(i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15 bool, sel bool4bit) bool {
 	return Multiplexer4to1(
@@ -57,7 +68,45 @@ func Multiplexer16to1(i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13
 }
 
 // 2to1マルチプレクサ(4bit)
+func Multiplexer2to1_4bit(a, b bool4bit, sel bool) bool4bit {
+	return bool4bit{
+		b3: Multiplexer2to1(a.b3, b.b3, sel),
+		b2: Multiplexer2to1(a.b2, b.b2, sel),
+		b1: Multiplexer2to1(a.b1, b.b1, sel),
+		b0: Multiplexer2to1(a.b0, b.b0, sel),
+	}
+}
+
+// 4to1マルチプレクサ(4bit)
+func Multiplexer4to1_4bit(a, b, c, d bool4bit, sel bool2bit) bool4bit {
+	return Multiplexer2to1_4bit(
+		Multiplexer2to1_4bit(a, b, sel.b0),
+		Multiplexer2to1_4bit(c, d, sel.b0),
+		sel.b1,
+	)
+}
 
 // 16to1マルチプレクサ(4bit)
+func Multiplexer16to1_4bit(i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15 bool4bit, sel bool4bit) bool4bit {
+	return Multiplexer4to1_4bit(
+		Multiplexer4to1_4bit(i0, i1, i2, i3, bool2bit{sel.b1, sel.b0}),
+		Multiplexer4to1_4bit(i4, i5, i6, i7, bool2bit{sel.b1, sel.b0}),
+		Multiplexer4to1_4bit(i8, i9, i10, i11, bool2bit{sel.b1, sel.b0}),
+		Multiplexer4to1_4bit(i2, i13, i14, i15, bool2bit{sel.b1, sel.b1}),
+		bool2bit{sel.b3, sel.b2},
+	)
+}
 
 // 16to1マルチプレクサ(8bit)
+func Multiplexer16to1_8bit(i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15 bool8bit, sel bool4bit) bool8bit {
+	return bool8bit{
+		b7: Multiplexer16to1(i0.b7, i1.b7, i2.b7, i3.b7, i4.b7, i5.b7, i6.b7, i7.b7, i8.b7, i9.b7, i10.b7, i11.b7, i12.b7, i13.b7, i14.b7, i15.b7, sel),
+		b6: Multiplexer16to1(i0.b6, i1.b6, i2.b6, i3.b6, i4.b6, i5.b6, i6.b6, i7.b6, i8.b6, i9.b6, i10.b6, i11.b6, i12.b6, i13.b6, i14.b6, i15.b6, sel),
+		b5: Multiplexer16to1(i0.b5, i1.b5, i2.b5, i3.b5, i4.b5, i5.b5, i6.b5, i7.b5, i8.b5, i9.b5, i10.b5, i11.b5, i12.b5, i13.b5, i14.b5, i15.b5, sel),
+		b4: Multiplexer16to1(i0.b4, i1.b4, i2.b4, i3.b4, i4.b4, i5.b4, i6.b4, i7.b4, i8.b4, i9.b4, i10.b4, i11.b4, i12.b4, i13.b4, i14.b4, i15.b4, sel),
+		b3: Multiplexer16to1(i0.b3, i1.b3, i2.b3, i3.b3, i4.b3, i5.b3, i6.b3, i7.b3, i8.b3, i9.b3, i10.b3, i11.b3, i12.b3, i13.b3, i14.b3, i15.b3, sel),
+		b2: Multiplexer16to1(i0.b2, i1.b2, i2.b2, i3.b2, i4.b2, i5.b2, i6.b2, i7.b2, i8.b2, i9.b2, i10.b2, i11.b2, i12.b2, i13.b2, i14.b2, i15.b2, sel),
+		b1: Multiplexer16to1(i0.b1, i1.b1, i2.b1, i3.b1, i4.b1, i5.b1, i6.b1, i7.b1, i8.b1, i9.b1, i10.b1, i11.b1, i12.b1, i13.b1, i14.b1, i15.b1, sel),
+		b0: Multiplexer16to1(i0.b0, i1.b0, i2.b0, i3.b0, i4.b0, i5.b0, i6.b0, i7.b0, i8.b0, i9.b0, i10.b0, i11.b0, i12.b0, i13.b0, i14.b0, i15.b0, sel),
+	}
+}
--- multiplexer/multi_plexer_test.go
+++ multiplexer/multi_plexer_test.go
@@ -1,6 +1,7 @@
 package multiplexer
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -128,6 +129,291 @@ func TestMultiplexer16to1(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Multiplexer16to1(tt.args.i0, tt.args.i1, tt.args.i2, tt.args.i3, tt.args.i4, tt.args.i5, tt.args.i6, tt.args.i7, tt.args.i8, tt.args.i9, tt.args.i10, tt.args.i11, tt.args.i12, tt.args.i13, tt.args.i14, tt.args.i15, tt.args.sel); got != tt.want {
 				t.Errorf("Multiplexer16to1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultiplexer2to1_4bit(t *testing.T) {
+	type args struct {
+		a   bool4bit
+		b   bool4bit
+		sel bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool4bit
+	}{
+		{
+			"Multiplexer2to1_4bit sel=falseの時はaを返す",
+			args{bool4bit{true, true, true, true}, bool4bit{false, false, false, false}, false},
+			bool4bit{true, true, true, true},
+		},
+		{
+			"Multiplexer2to1_4bit sel=trueの時はbを返す",
+			args{bool4bit{false, false, false, false}, bool4bit{true, true, true, true}, true},
+			bool4bit{true, true, true, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Multiplexer2to1_4bit(tt.args.a, tt.args.b, tt.args.sel); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Multiplexer2to1_4bit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultiplexer16to1_4bit(t *testing.T) {
+	type args struct {
+		i0  bool4bit
+		i1  bool4bit
+		i2  bool4bit
+		i3  bool4bit
+		i4  bool4bit
+		i5  bool4bit
+		i6  bool4bit
+		i7  bool4bit
+		i8  bool4bit
+		i9  bool4bit
+		i10 bool4bit
+		i11 bool4bit
+		i12 bool4bit
+		i13 bool4bit
+		i14 bool4bit
+		i15 bool4bit
+		sel bool4bit
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool4bit
+	}{
+		{
+			"Multiplexer16to1_4bit 0000の時はi0を返す",
+			args{
+				bool4bit{true, true, true, true},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+			},
+			bool4bit{true, true, true, true},
+		},
+		{
+			"Multiplexer16to1_4bit 0101の時はi5を返す",
+			args{
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{true, true, true, true},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, true, false, true},
+			},
+			bool4bit{true, true, true, true},
+		},
+		{
+			"Multiplexer16to1_4bit 1010の時はi10を返す",
+			args{
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{true, true, true, true},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{true, false, true, false},
+			},
+			bool4bit{true, true, true, true},
+		},
+		{
+			"Multiplexer16to1_4bit 1111の時はi15を返す",
+			args{
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{false, false, false, false},
+				bool4bit{true, true, true, true},
+				bool4bit{true, true, true, true},
+			},
+			bool4bit{true, true, true, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Multiplexer16to1_4bit(tt.args.i0, tt.args.i1, tt.args.i2, tt.args.i3, tt.args.i4, tt.args.i5, tt.args.i6, tt.args.i7, tt.args.i8, tt.args.i9, tt.args.i10, tt.args.i11, tt.args.i12, tt.args.i13, tt.args.i14, tt.args.i15, tt.args.sel); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Multiplexer16to1_4bit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultiplexer16to1_8bit(t *testing.T) {
+	type args struct {
+		i0  bool8bit
+		i1  bool8bit
+		i2  bool8bit
+		i3  bool8bit
+		i4  bool8bit
+		i5  bool8bit
+		i6  bool8bit
+		i7  bool8bit
+		i8  bool8bit
+		i9  bool8bit
+		i10 bool8bit
+		i11 bool8bit
+		i12 bool8bit
+		i13 bool8bit
+		i14 bool8bit
+		i15 bool8bit
+		sel bool4bit
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool8bit
+	}{
+		{
+			"Multiplexer16to1_8bit 0000の時はi0を返す",
+			args{
+				bool8bit{true, true, true, true, true, true, true, true},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool4bit{false, false, false, false},
+			},
+			bool8bit{true, true, true, true, true, true, true, true},
+		},
+		{
+			"Multiplexer16to1_8bit 0101の時はi5を返す",
+			args{
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{true, true, true, true, true, true, true, true},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool4bit{false, true, false, true},
+			},
+			bool8bit{true, true, true, true, true, true, true, true},
+		},
+		{
+			"Multiplexer16to1_8bit 1010の時はi10を返す",
+			args{
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{true, true, true, true, true, true, true, true},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool4bit{true, false, true, false},
+			},
+			bool8bit{true, true, true, true, true, true, true, true},
+		},
+		{
+			"Multiplexer16to1_8bit 1111の時はi15を返す",
+			args{
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{false, false, false, false, false, false, false, false},
+				bool8bit{true, true, true, true, true, true, true, true},
+				bool4bit{true, true, true, true},
+			},
+			bool8bit{true, true, true, true, true, true, true, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Multiplexer16to1_8bit(tt.args.i0, tt.args.i1, tt.args.i2, tt.args.i3, tt.args.i4, tt.args.i5, tt.args.i6, tt.args.i7, tt.args.i8, tt.args.i9, tt.args.i10, tt.args.i11, tt.args.i12, tt.args.i13, tt.args.i14, tt.args.i15, tt.args.sel); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Multiplexer16to1_8bit() = %v, want %v", got, tt.want)
 			}
 		})
 	}
