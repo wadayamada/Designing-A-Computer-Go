e8c3dfe999a08f70b72ea64ea8c53423bcf5af6c
wada <wada0421514@gmail.com>
2024-06-28 18:53:21 +0900

    opecode, immediateの取り出しはそのまま取り出すだけで良い

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -27,8 +27,8 @@ func (c *CPU) Write(a, b, ip, out, cf multiplexer.Bool4bit) {
 func (c *CPU) Run(romInterface rom.RomInterface) {
 	for {
 		data := romInterface.Get(c.RegisterIP.D)
-		// TODO: Bool8bitからBool4bit, Bool4bitに変換する
-		opecode, imm := multiplexer.Bool4bit{B3: data.B7, B2: data.B6, B1: data.B5, B0: data.B4}, multiplexer.Bool4bit{B3: data.B3, B2: data.B2, B1: data.B1, B0: data.B0}
+		opecode := multiplexer.Bool4bit{B3: data.B7, B2: data.B6, B1: data.B5, B0: data.B4}
+		imm := multiplexer.Bool4bit{B3: data.B3, B2: data.B2, B1: data.B1, B0: data.B0}
 		a, b, ip, out, cf := c.ALUInterface.Run(opecode, imm, c.RegisterA.D, c.RegisterB.D, c.RegisterIP.D, c.RegisterOut.D, c.RegisterCF.D)
 		c.Write(a, b, ip, out, cf)
 	}
